fa567d2c8ba775641da4cc63d4e0b7a41a223c7b
MINAqwq <84045544+MINAqwq@users.noreply.github.com>
2023-05-27 14:22:47 +0200

    modified license comment

--- src/Server/TcpServer.hpp
+++ src/Server/TcpServer.hpp
@@ -1,3 +1,4 @@
+// This file is part of the Lyli Backend
 // File:    Server/TcpServer.hpp
 // Author:  Mina <mina@upndevelopment.de>
 // Date:    26 May 2032
@@ -15,7 +16,7 @@
 // You should have received a copy of the GNU General Public License along
 // with this program. If not, see <https://www.gnu.org/licenses/>.
 //
-// (c) UPN
+// (c) 2023 UPN
 //
 
 #pragma once
